ad8cc353c0f850814e06431dbb27b3e82a3cb932
Gabrielmota7 <82525773+Gabrielmota7@users.noreply.github.com>
2021-05-28 15:04:48 -0300

    Colocando o Tema

--- model/Postagem.java
+++ model/Postagem.java
@@ -8,7 +8,11 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
@@ -30,7 +34,12 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
-
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
+	
 	public long getId() {
 		return id;
 	}
@@ -62,6 +71,13 @@ public class Postagem {
 	public void setDate(Date date) {
 		this.date = date;
 	}
+	
+	public Tema getTema() {
+		return tema;
+	}
 
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 	
 }
